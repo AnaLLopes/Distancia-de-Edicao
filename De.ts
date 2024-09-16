177cb418e7d4d36cb3ae1929e1a7d6806c83ab88
FeFeFarias05 <fernanda.farias05@hotmail.com>
2024-09-16 19:50:31 -0300

    problema

--- De.ts
+++ De.ts
@@ -1,20 +1,25 @@
 function ED (S: string, T: string, i: number, j: number){
+    let count =+1;
     //caso base
     if(S.length === 0 && T.length === 0){
         return 0;
     }
     if(S.length === 0 && T.length !== 0){
-        return T;
+        return j;
     }
     if(T.length === 0 && S.length !== 0){
-        return S;
+        return i;
     }
     //chamadas recursivas
     if(S[i] === T[j]){
         return ED(S, T, i-1, j-1);
     }
-    ED(S, T, i-1, j-1) //substituicso
-    let substituica  
-    ED(S, T, i, j-1) //insercao
-    ED(S, T, i-1, j) //remocao
+    
+    let substituica = ED(S, T, i-1, j-1) + 1; //substituicso
+    let inserçao = ED(S, T, i, j-1) + 1; //insercao
+    let remocao = ED(S, T, i-1, j) + 1;//remocao
+    console.log(count);
+    return Math.min(substituica, inserçao, remocao);
 }
+
+ED("casa", "carta", 3, 3);
